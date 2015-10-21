2bc2d6b40d652ec5732d6fc0ad53a9f74c165a02
h3nnn4n <uber.renan@gmail.com>
2015-10-21 08:38:52 -0200

    Wip

--- magic.cpp
+++ magic.cpp
@@ -6,7 +6,12 @@ class vector{
         double x, y, z;
 
     public:
-        vector(double xi, double yi, double zi) : x(xi), y(yi), z(zi) {}
+        vector(double xi = 0, double yi = 0, double zi = 0){
+            x = xi;
+            y = yi;
+            z = zi; 
+        }
+
         vector(const vector &u){
             x = u.x;
             y = u.y;
@@ -58,6 +63,12 @@ class vector{
             return;
         }
 
+        vector cross (vector u){
+            return vector(y * u.z - z * u.y,
+                          z * u.x - x * u.z,
+                          x * u.y - y * u.x);
+        }
+
         vector normalized(){
             vector w = *this / norm();
             return w.copy();
@@ -73,7 +84,11 @@ class color{
 
     public:
         double r, g, b;
-        color(double rr, double gg, double bb) : r(rr), g(gg), b(bb) {}
+        color(double rr = 0, double gg = 0, double bb = 0){
+            r = rr;
+            g = gg;
+            b = bb;   
+        }
 
 };
 
@@ -100,7 +115,6 @@ class plane{
 };
 
 plane planes[] = {
-    plane( vector(-1, 0, -1), vector(0, 1, 0), color(0.999, 0.999, 0.909)),
     plane( vector(-1, 0, -1), vector(0, 1, 0), color(0.999, 0.999, 0.909))
 };
 
@@ -133,6 +147,18 @@ double truncate(double w){
 }
 
 int main(){
+    int screenx = 800;
+    int screeny = 600;
+
+    int steps = 10;
+
+    path cam(vector(5, 5, 5), vector(0, 0, 0).normalized());
+    vector dx = vector(screenx*.5135/screeny);
+    vector dy = dx.cross(cam.end) * .5135;
+
+    color r;
+
+    color *image = new color[screenx * screeny];
 
     return 0;
 }
