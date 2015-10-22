600509120bcf80224f9dc983ef75fcb01053026f
h3nnn4n <uber.renan@gmail.com>
2015-10-22 11:54:23 -0200

    hue

--- magic.cpp
+++ magic.cpp
@@ -6,9 +6,10 @@ int gammaCorrection(double);
 
 class vector{
     private:
-        double x, y, z;
 
     public:
+        double x, y, z;
+
         vector(double xi = 0, double yi = 0, double zi = 0){
             x = xi;
             y = yi;
@@ -50,7 +51,7 @@ class vector{
         }
 
         double dot(vector u){
-            return x * u.x + y + u.y + z * u.z;
+            return x * u.x + y * u.y + z * u.z;
         }
 
         double norm(){
@@ -121,11 +122,30 @@ class plane{
         color  c;
         color  emission;
 
+        plane(vector p, vector n) : normal(n), point(p) {}
         plane(vector p, vector n, color cc) : normal(n), point(p), c(cc) {}
         plane(vector p, vector n, color cc, color ee) : normal(n), point(p), c(cc), emission(ee) {}
 
         double intersect(path p){
-            return ((point - p.begin).dot(normal)) / p.end.dot(normal);
+            vector l0 = p.begin + p.end * 4.2; // Magic number
+            double u = (point - l0).dot(normal);
+            double v = p.end.dot(normal);
+
+            double d = -(p.end.x * normal.x + p.end.y * normal.y + p.end.z * normal.z);
+            double x, y, z;
+            x = 2;
+            y = 3;
+            z = 0; // TODO ARRUMAR 
+
+            if ( v == 0 ){
+                std::cout << "Paralelo" << '\n';
+                return 0.0;
+            } else if ( u == 0 ){
+                std::cout << "igual" << '\n';
+                return 0.0; 
+            } else {
+                return (u) / v;
+            }
         }
 };
 
@@ -194,39 +214,53 @@ color tracer(path ray, int iter){
 }
 
 int main(){
-    int screenx = 800;
-    int screeny = 600;
+    path  v(vector(0, 2, 0), vector(0, 0, 0));
+    plane p(vector(0, 2, 0), vector(0, 0, 0));
 
-    int steps = 10;
+    //vector a(1, 2, 3);
+    //vector b(4,-5, 6);
 
-    path cam(vector(5, 5, 5), vector(0, 0, 0).normalized()); // Camera position: pos and direction
-    vector dx = vector(screenx * .5135 / screeny); // horizontal increment
-    vector dy = dx.cross(cam.end) * .5135;         // Vertical increment
+    //std::cout << a.dot(b) << '\n';
 
-    color r;
-
-    color *image = new color[screenx * screeny];
-
-    for (int y = 0; y < screeny; y++){
-        int x;
-        for (x = 0; x < screenx; x++){
-            vector ray = dx * (x/screenx + 0.5) +
-                         dy * (y/screeny + 0.5) + cam.end;
-
-            r = r + tracer(path(cam.end + ray*140, ray.normalized()), 5);
-        }
-
-        image[x + y*screenx] = image[x + y*screenx] + r.norm();
-    }
-
-    FILE *f = fopen("image.ppm", "w");         // PPM cancer
-    fprintf(f, "P3\n%d %d\n%d\n", screenx, screeny, 255);
-
-    for (int i = 0; i < screenx * screeny; i++) {
-        fprintf(f,"%d %d %d ", gammaCorrection(image[i].r),
-                               gammaCorrection(image[i].g),
-                               gammaCorrection(image[i].b));
-    }
+    std::cout << p.intersect(v) << '\n';
 
     return 0;
 }
+
+//int main(){
+    //int screenx = 800;
+    //int screeny = 600;
+
+    //int steps = 10;
+
+    //path cam(vector(5, 5, 5), vector(0, 0, 0).normalized()); // Camera position: pos and direction
+    //vector dx = vector(screenx * .5135 / screeny); // horizontal increment
+    //vector dy = dx.cross(cam.end) * .5135;         // Vertical increment
+
+    //color r;
+
+    //color *image = new color[screenx * screeny];
+
+    //for (int y = 0; y < screeny; y++){
+        //int x;
+        //for (x = 0; x < screenx; x++){
+            //vector ray = dx * (x/screenx + 0.5) +
+                         //dy * (y/screeny + 0.5) + cam.end;
+
+            //r = r + tracer(path(cam.end + ray*140, ray.normalized()), 5);
+        //}
+
+        //image[x + y*screenx] = image[x + y*screenx] + r.norm();
+    //}
+
+    //FILE *f = fopen("image.ppm", "w");         // PPM cancer
+    //fprintf(f, "P3\n%d %d\n%d\n", screenx, screeny, 255);
+
+    //for (int i = 0; i < screenx * screeny; i++) {
+        //fprintf(f,"%d %d %d ", gammaCorrection(image[i].r),
+                               //gammaCorrection(image[i].g),
+                               //gammaCorrection(image[i].b));
+    //}
+
+    //return 0;
+//}
