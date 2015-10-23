da666560f7995f9de5dfb9c14950902a89dd1d23
h3nnn4n <uber.renan@gmail.com>
2015-10-23 00:27:03 -0200

    Wip

--- magic.cpp
+++ magic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <cstdio>
 #include <cmath>
 
@@ -13,7 +14,7 @@ class vector{
         vector(double xi = 0, double yi = 0, double zi = 0){
             x = xi;
             y = yi;
-            z = zi; 
+            z = zi;
         }
 
         vector(const vector &u){
@@ -91,7 +92,11 @@ class color{
         color(double rr = 0, double gg = 0, double bb = 0){
             r = rr;
             g = gg;
-            b = bb;   
+            b = bb;
+        }
+
+        void print(){
+            std::cout << "( " << r << ", " << g << ", " << b << " )\n";
         }
 
         color operator + (color v){
@@ -99,9 +104,16 @@ class color{
         }
 
         color norm(){
-            return color(gammaCorrection(r), gammaCorrection(g), gammaCorrection(b)); 
+            return color(gammaCorrection(r), gammaCorrection(g), gammaCorrection(b));
         }
 
+        color operator * (double k){
+            return color(r * k, g * k, b * k);
+        }
+
+        color operator * (color v){
+            return color(r * v.r, g * v.g, b * v.b);
+        }
 };
 
 class path{
@@ -111,6 +123,14 @@ class path{
         vector begin, end;
 
         path(vector b, vector e) : begin(b), end(e) {}
+
+        vector getPoint(double t){
+            return begin + (end - begin) * t;
+        }
+
+        vector getDir(){
+            return end - begin;
+        }
 };
 
 class plane{
@@ -127,40 +147,48 @@ class plane{
         plane(vector p, vector n, color cc, color ee) : normal(n), point(p), c(cc), emission(ee) {}
 
         double intersect(path p){
-            vector l0 = p.begin + p.end * 4.2; // Magic number
-            double u = (point - l0).dot(normal);
-            double v = p.end.dot(normal);
+            vector u = p.getDir();      // B - A
+            vector w = p.begin - point;
 
-            double d = -(p.end.x * normal.x + p.end.y * normal.y + p.end.z * normal.z);
-            double x, y, z;
-            x = 2;
-            y = 3;
-            z = 0; // TODO ARRUMAR 
+            double D =  normal.dot(u);
+            double N = -normal.dot(w);
 
-            if ( v == 0 ){
-                std::cout << "Paralelo" << '\n';
-                return 0.0;
-            } else if ( u == 0 ){
-                std::cout << "igual" << '\n';
-                return 0.0; 
-            } else {
-                return (u) / v;
+            if ( D == 0 ){
+                if ( N == 0 ){
+                    std::cout << "Inside" << '\n';
+                    return 0.0;
+                } else {
+                    std::cout << "Parallel" << '\n';
+                    return 0.0;
+                }
             }
+
+            double intersec = N / D;
+
+            if ( intersec < 0 ){
+                //std::cout<<"aksadjkas\n";
+                return 0.0;
+            }
+
+            return intersec;
         }
 };
 
 plane planes[] = {
-    plane( vector(-1, 0, -1), vector(0, 1, 0), color(0.999, 0.999, 0.909), color(.5, .2, .99))
+    plane( vector( 0, 0, 0), vector(1, 0, 0), color(.9, .0, .0), color(0.329, 0.249, 0.959)),
+    plane( vector( 0, 0, 0), vector(0, 1, 0), color(.0, .9, .0), color(0.529, 0.429, 0.429)),
+    plane( vector( 0, 0, 0), vector(0, 0, 1), color(.0, .0, .9), color(0.299, 0.949, 0.209))
 };
 
 bool intersects(path p, int *n, double *dist){
     double size = sizeof(planes) / sizeof(plane);
     double distance;
     double dold = 1<<20;
+          *dist = 1<<20;
 
     for(int i = 0 ; i < size ; i++){
         dold = planes[i].intersect(p);
-        if (dold && dold < *dist){
+        if (dold > 0 && dold < *dist){
             *n = i;
             *dist = dold;
         }
@@ -182,7 +210,7 @@ double truncate(double w){
 }
 
 int gammaCorrection(double x){
-    return int(pow( truncate(x), 1 / 2.2) * 255 + .5);
+    return (pow( truncate(x), 1.0 / 2.2) * 255.0 + .5 );
 }
 
 color tracer(path ray, int iter){
@@ -196,7 +224,8 @@ color tracer(path ray, int iter){
     plane *target = &planes[id];
 
     vector x     = ray.begin + ray.end * distance;
-    vector norma = (x - target->normal);
+    vector norma = (x - target->point).normalized();
+    //vector norma = (target->normal);
     color f      = target->c;
     vector nl;
 
@@ -206,61 +235,103 @@ color tracer(path ray, int iter){
         nl = norma * -1.0;
     }
 
-    double maxReflex = 0.0;
+    double p = f.r > f.g && f.r > f.b ? f.r : f.g > f.b ? f.g : f.b; // max refl
 
-    
+    if (iter-- > 0){
+        if ( drand48() < p ){
+            f = f * (1 / p);
+        } else {
+            return target->emission;
+        }
+    }
 
-    return color(1, .7, .12);
+    if ( iter < -15 ){
+        return color();
+    }
+
+
+    // Mirror reflection not working
+    //return target->emission + f * (tracer(path(x, ((ray.begin *-1) + (x * 2) +target->normal * (ray.begin - x).dot(target->normal) * 2.0 ).normalized() ), iter));
+
+    //double r1  = 2 * M_PI * drand48();
+    //double r2  = drand48();
+    //double r2s = sqrt(r2);
+
+    //vector w = nl;
+    //vector u = ((fabs(w.x) > .1 ? vector(0,1) : vector(1)).cross(w)).norm();
+    //vector v = w.cross(u);
+    //vector d = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm();
+
+    //return target->emission + f * (tracer(path(x, d), iter));
+
+    //return target->emission + f * (tracer(path(x, ray.end - target->normal * 2 * (target->normal).dot(ray.end)), iter));
+    return target->emission + f * (tracer(path(x, x - target->normal * 2 * (target->normal).dot(x) + vector(drand48(),drand48(),drand48())), iter));
 }
 
 int main(){
-    path  v(vector(0, 2, 0), vector(0, 0, 0));
-    plane p(vector(0, 2, 0), vector(0, 0, 0));
+    //plane  p(vector(0, 0, 0), vector(1, 0, 0));
+    //vector wx(0.2, 0.2, 0.2);
+    //path   u(vector(1, 1, 1)+wx, vector(0, 0, 0)+wx);
 
-    //vector a(1, 2, 3);
-    //vector b(4,-5, 6);
+    //vector x = u.getPoint(p.intersect(u));
 
-    //std::cout << a.dot(b) << '\n';
+    //x.print();
 
-    std::cout << p.intersect(v) << '\n';
+    //vector w = u.end - p.normal * 2* p.normal.dot(u.end);
+
+    //w.print();
+
+    //return 0;
+
+    int screenx = 80;
+    int screeny = 60;
+
+    int steps = 10;
+
+    path cam(vector(5, 5, 1.6),
+             vector(1, 1, 1).normalized()); // cam pos, dir
+
+
+    //path cam(vector(15, 15, 15), vector(10, 10, 10).normalized()); // Camera position: pos and direction
+    vector dx = vector(screenx * .5135 / screeny); // horizontal increment
+    vector dy = dx.cross(cam.end) * .5135;         // Vertical increment
+
+    color r;
+
+    color *image = new color[screenx * screeny];
+
+    for (int y = 0; y < screeny; y++){
+        int x;
+        for (x = 0; x < screenx; x++){
+            r = color();
+            vector ray = dx * ((double)x/screenx + 0.5) +
+                         dy * ((double)y/screeny + 0.5) + cam.end;
+
+            int id = -1;
+            double dist = 1<<20 ;
+            bool ya = intersects(path(cam.begin, ray), &id, &dist);
+            std::cout << " id = " << id << " dist = " << dist << '\n';
+
+            //continue;
+
+            r = r + tracer(path(cam.begin + ray*140, ray.normalized()), 5);
+            //r.norm().print();
+            image[x + y*screenx] = image[x + y*screenx] + r.norm();
+            //image[x + y*screenx].print();
+        }
+
+    }
+
+    std::cout << "Done\n";
+
+    FILE *f = fopen("image.ppm", "w");         // PPM cancer
+    fprintf(f, "P3\n%d %d\n%d\n", screenx, screeny, 255);
+
+    for (int i = 0; i < screenx * screeny; i++) {
+        fprintf(f,"%d %d %d ", (image[i].r),
+                               (image[i].g),
+                               (image[i].b));
+    }
 
     return 0;
 }
-
-//int main(){
-    //int screenx = 800;
-    //int screeny = 600;
-
-    //int steps = 10;
-
-    //path cam(vector(5, 5, 5), vector(0, 0, 0).normalized()); // Camera position: pos and direction
-    //vector dx = vector(screenx * .5135 / screeny); // horizontal increment
-    //vector dy = dx.cross(cam.end) * .5135;         // Vertical increment
-
-    //color r;
-
-    //color *image = new color[screenx * screeny];
-
-    //for (int y = 0; y < screeny; y++){
-        //int x;
-        //for (x = 0; x < screenx; x++){
-            //vector ray = dx * (x/screenx + 0.5) +
-                         //dy * (y/screeny + 0.5) + cam.end;
-
-            //r = r + tracer(path(cam.end + ray*140, ray.normalized()), 5);
-        //}
-
-        //image[x + y*screenx] = image[x + y*screenx] + r.norm();
-    //}
-
-    //FILE *f = fopen("image.ppm", "w");         // PPM cancer
-    //fprintf(f, "P3\n%d %d\n%d\n", screenx, screeny, 255);
-
-    //for (int i = 0; i < screenx * screeny; i++) {
-        //fprintf(f,"%d %d %d ", gammaCorrection(image[i].r),
-                               //gammaCorrection(image[i].g),
-                               //gammaCorrection(image[i].b));
-    //}
-
-    //return 0;
-//}
